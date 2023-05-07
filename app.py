79c71c789b80f49b7d4bb9265767d82b841c43a3
Finalcreature <finalnadav@gmail.com>
2023-05-07 13:20:58 +0300

    downloading files work correctly

--- app.py
+++ app.py
@@ -4,47 +4,68 @@ import re
 import random
 import pandas as pd
 
-#url = 'https://www.apkmirror.com/'
+user_input = input('please provide a url: ')
+
+
+
+url = 'https://www.apkmirror.com'
 user_agents_list = [
     'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
 ]
 
-new_url = 'https://www.apkmirror.com/apk/chess-com/chess-play-learn/chess-play-learn-4-5-16-googleplay-release/'
+new_url = user_input  or 'https://www.apkmirror.com/uploads/?appcategory=eufy-security'
+
 
 
 req = requests.get(new_url, headers={'User-Agent': random.choice(user_agents_list)})
 
 
 soup = BeautifulSoup(req.text, 'lxml')
-soup
 
-result = soup.find_all('h5',class_='appRowTitle')
 
-result = soup.find('div',string='All Releases ')
-releases = result.parent
+app_table = soup.find('div', class_='widget widget_appmanager_recentpostswidget')
+
+app_rows = app_table.find_all('div', class_='appRow')
+
+
+save_path = 'D:\Studies\Web scraping\MirrorAPK'
+
+
+def createUrl(link):
+    return url + link
+
+
+
+for row in app_rows:
+    title = row.find('h5', class_='appRowTitle').text.strip()
+    if(title is not None):
+        
+        download_link = createUrl(row.find('a', class_='downloadLink').get('href')) 
+        print(download_link)
+        break
+        req = requests.get(download_link, headers={'User-Agent': random.choice(user_agents_list)})
+        soup = BeautifulSoup(req.text, 'lxml')
+        download_button = soup.find('div', class_= 'table topmargin variants-table').find('a', 'accent_color').get('href')
+        new_link =  url + download_button     
+        req = requests.get(new_link, headers={'User-Agent': random.choice(user_agents_list)})
+        soup = BeautifulSoup(req.text, 'lxml')
+        final_button = url + soup.find('a', class_='downloadButton').get('href')
+        req = requests.get(final_button,stream=True, headers={'User-Agent': random.choice(user_agents_list)})
+        if req.status_code == 200:
+            filename = save_path+'\\'+title+'.apk'
+            with open(filename, 'wb') as f:
+                for chunk in req.iter_content(chunk_size=1024):
+                    f.write(chunk)
+                    print('File downloaded successfully')
+        else:
+            print('Failed to download file')
+  
+        
 
     
-versions = soup.find_all(class_='infoSlide t-height')
-versions
 
+# table = pd.DataFrame({'Version': all_versions})
 
-all_versions = []
-
-
-for v in versions:
-    v = v.find_all('p')[0].text
-    pattern = r':(.+)-'
-    match = re.search(pattern, v)
-    if match:
-        v = match.group(1)
-       
-    
-    all_versions.append(v)
-    
-    
-
-table = pd.DataFrame({'Version': all_versions})
-
-table.to_csv('D:\Studies\Web scraping\MirrorAPK/table.csv')
+# table.to_csv('D:\Studies\Web scraping\MirrorAPK/table.csv')
